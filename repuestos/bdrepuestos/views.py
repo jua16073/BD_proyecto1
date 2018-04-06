0ce4293672a785f98415e131dae2c3a926d3302a
Unknown <caarroyavepadilla@gmail.com>
2018-04-06 07:15:36 -0600

    vainas
    vainas locas

--- repuestos/bdrepuestos/views.py
+++ repuestos/bdrepuestos/views.py
@@ -53,6 +53,8 @@ def anadirProducto(request):
     return render(request, 'bdrepuestos/repuestos.html', {'paso':0, 'form': ContactForm(modelo=2), 'things': Producto.objects.all()})
 
 
+
+
 #Se edita un PRODUCTO que ya este en la base de datos
 def editarProducto(request, producto_id):
     inst = get_object_or_404(Producto, pk=producto_id)
@@ -113,6 +115,51 @@ def home(request):
     else:
         return render(request, 'bdrepuestos/home.html', {'form': anadirVentaForm(request.POST)})
     return redirect(editarVenta, pls = local)
+
+
+def resumen():
+    x=connection.cursor()
+    try:
+        x.execute("SELECT MAX(total_ventas) FROM bdrepuestos_vendedor")
+        max = x.fetchone()[0]
+        x.execute("SELECT nombre,total_ventas FROM bdrepuestos_vendedor WHERE total_ventas="+str(max))
+        cosas = x.fetchall()
+    except ValueError:
+        cosas = Vendedor.objects.all()
+    return cosas
+
+def resumen1():
+    x=connection.cursor()
+    try:
+        x.execute("SELECT SUM(total_ventas) FROM bdrepuestos_vendedor")
+        cosas = x.fetchall()
+    except ValueError:
+        cosas = Vendedor.objects.all()
+    return cosas
+
+
+def resumen2():
+    x=connection.cursor()
+    try:
+        x.execute("SELECT COUNT(tipo),tipo FROM bdrepuestos_cliente GROUP BY tipo")
+        cosas = x.fetchall()
+    except ValueError:
+        cosas = Vendedor.objects.all()
+    return cosas
+
+def resumen3():
+    x=connection.cursor()
+    try:
+        x.execute("SELECT COUNT(l.producto_id),p.nombre FROM bdrepuestos_lineaventa l, bdrepuestos_producto p WHERE l.producto_id = p.id GROUP BY p.nombre ORDER BY COUNT(l.producto_id) DESC LIMIT 1")
+        cosas = x.fetchall()
+    except ValueError:
+        cosas = Vendedor.objects.all()
+    return cosas
+
+
+
+def resumenes(request):
+    return render(request, "bdrepuestos/resumenes.html",{'res1': resumen(),'res2':resumen1(),'res3': resumen2(), 'res4': resumen3()})
 
 def editarVenta(request, pls):
     try:
--- repuestos/repuestos/settings.py
+++ repuestos/repuestos/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bdrepuestos',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
